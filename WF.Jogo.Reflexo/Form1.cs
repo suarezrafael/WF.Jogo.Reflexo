3ac3e0fb9bcaa1c75cfd53c16ac0b00029830237
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2025-02-13 01:32:32 -0300

    sdfg

--- WF.Jogo.Reflexo/Form1.cs
+++ WF.Jogo.Reflexo/Form1.cs
@@ -4,8 +4,8 @@ namespace WF.Jogo.Reflexo
 {
     public partial class Form1 : Form
     {
-        private Button btnStart;
-        private Button btnTarget;
+        private Button btnIniciar;
+        private Button btnAlvo;
         private System.Windows.Forms.Timer timer;
         private Random random;
         private Stopwatch stopwatch;
@@ -16,45 +16,45 @@ namespace WF.Jogo.Reflexo
             this.Size = new Size(400, 400);
             this.StartPosition = FormStartPosition.CenterScreen;
 
-            btnStart = new Button() { Text = "Iniciar", Size = new Size(100, 50), Location = new Point(150, 150) };
-            btnStart.Click += StartGame;
-            this.Controls.Add(btnStart);
+            btnIniciar = new Button() { Text = "Iniciar", Size = new Size(100, 50), Location = new Point(150, 150) };
+            btnIniciar.Click += IniciarJogo;
+            this.Controls.Add(btnIniciar);
 
-            btnTarget = new Button() { Size = new Size(60, 60), BackColor = Color.Red, Visible = false };
-            btnTarget.Click += TargetClicked;
-            this.Controls.Add(btnTarget);
+            btnAlvo = new Button() { Size = new Size(60, 60), BackColor = Color.Red, Visible = false };
+            btnAlvo.Click += btnAlvoClick;
+            this.Controls.Add(btnAlvo);
 
             timer = new System.Windows.Forms.Timer();
-            timer.Tick += ShowTarget;
+            timer.Tick += MostrarBotaoAlvo;
 
             random = new Random();
             stopwatch = new Stopwatch();
         }
-        private void StartGame(object sender, EventArgs e)
+        private void IniciarJogo(object sender, EventArgs e)
         {
-            btnStart.Enabled = false;
+            btnIniciar.Enabled = false;
             timer.Interval = random.Next(1000, 3000);
             timer.Start();
         }
 
-        private void ShowTarget(object sender, EventArgs e)
+        private void MostrarBotaoAlvo(object sender, EventArgs e)
         {
             timer.Stop();
 
             int x = random.Next(50, this.ClientSize.Width - 70);
             int y = random.Next(50, this.ClientSize.Height - 70);
 
-            btnTarget.Location = new Point(x, y);
-            btnTarget.Visible = true;
+            btnAlvo.Location = new Point(x, y);
+            btnAlvo.Visible = true;
 
             stopwatch.Restart();
         }
-        private void TargetClicked(object sender, EventArgs e)
+        private void btnAlvoClick(object sender, EventArgs e)
         {
             stopwatch.Stop();
-            btnTarget.Visible = false;
+            btnAlvo.Visible = false;
             MessageBox.Show($"Seu tempo de reação: {stopwatch.ElapsedMilliseconds} ms", "Resultado");
-            btnStart.Enabled = true;
+            btnAlvo.Enabled = true;
         }
     }
 }
